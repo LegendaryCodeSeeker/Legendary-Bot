48e6338541dc81073a8d7e16811ea79dc021d014
LegendaryCodeSeeker <seeker.of.funz@gmail.com>
2017-03-09 23:34:25 +0000

    Super Tiny Update

--- Legendary_bot/gears/minigemas.py
+++ Legendary_bot/gears/minigemas.py
@@ -7,16 +7,17 @@ import math
 import asyncio
 import sqlite3
 
+lottoOn = False
+
 class MG():
     def __init__(self, bot):
         self.bot = bot
-        lottoOn = False
     #Lottery
     #Lottery start 
     @commands.command(pass_context=True)
     async def lotto_start(self, ctx, lottotime:int, lottodrops:int):
         if lottoOn == True:
-            await self.bot.send_message(ctx.message.channel, 'Sorry but theres a lottery already going on.\n Do `||lotto` for more info')
+            await self.bot.send_message(ctx.message.channel, 'Sorry but theres a lottery already going on.\n Do ||lotto for more info')
         else:
             if lottotime < 300:
                  await self.bot.send_message(ctx.message.channel, 'Lottery must last 5 mins or longer')

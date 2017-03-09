9a16042c8031be8109e242516c0722223eb84b6b
LegendaryCodeSeeker <seeker.of.funz@gmail.com>
2017-03-09 03:08:53 +0000

    Small test update

--- Legendary_bot/gears/minigemas.py
+++ Legendary_bot/gears/minigemas.py
@@ -14,7 +14,7 @@ class MG():
     async def lotto_start(self, ctx, lottotime:int):
         async def lotto_timer():
             await asyncio.sleep(lottotime)
-            await print("Lotto is over, picking winner.")
+            print("Lotto is over, picking winner.")
         self.bot.loop.create_task(lotto_timer())
 
 def setup(bot):

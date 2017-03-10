d8636a23ceb038d53d8c25cc3c21b387d56d323b
LegendaryCodeSeeker <seeker.of.funz@gmail.com>
2017-03-10 00:34:30 +0000

    Adding stuff

---
+++ Legendary_bot_Legacy/VA0.1HF.C/gears/minigemas.py
@@ -0,0 +1,37 @@
+#imports
+import discord
+from discord.ext import commands
+import time
+import datetime
+import math
+import asyncio
+import sqlite3
+
+lottoOn = False
+
+class MG():
+    def __init__(self, bot):
+        self.bot = bot
+    #Lottery
+    #Lottery start 
+    @commands.command(pass_context=True)
+    async def lotto_start(self, ctx, lottotime:int, lottodrops:int):
+        if lottoOn == True:
+            await self.bot.send_message(ctx.message.channel, 'Sorry but theres a lottery already going on.\n Do ||lotto for more info')
+        else:
+            if lottotime < 300:
+                 await self.bot.send_message(ctx.message.channel, 'Lottery must last 5 mins or longer')
+            else:
+                if lottodrops < 50:
+                    await self.bot.send_message(ctx.message.channel, 'You must start a Lottery With 50 or more Tea Drops in the pot.')
+                else:
+                    async def lotto_timer():
+                        starttime = datetime.datetime.now()
+                        lottoOn = True
+                        await asyncio.sleep(lottotime)
+                        lottoOn = False
+                        await self.bot.send_message(ctx.message.channel, 'The lottery is finished, the winning numbers belong too...')
+                    self.bot.loop.create_task(lotto_timer())
+
+def setup(bot):
+    bot.add_cog(MG(bot))
---
+++ Legendary_bot_Legacy/VA0.1HF.C/legendbot.py
@@ -0,0 +1,76 @@
+#Legendary Bot V0.2(Alpha)[HF.A] @23:07:00 3/6/17
+
+#Imports
+from discord.ext import commands
+import discord
+
+#Autoloading Items
+auto_load = ["gears.sraeg","gears.minigemas"]
+
+#Main
+bot = commands.Bot(command_prefix='||', description="Testing")
+
+#Bot Awaiting Orders
+@bot.event
+async def on_ready():
+    print('The Legend Begins.')
+    print('The Legend is about {} with the knowlegd of the scroll called {}'.format(bot.user.name, bot.user.id))
+    print('-_-_-_-_-_-_-_-_-_-_-')
+    await bot.change_presence(game=discord.Game(name="DiscordHackerBot.exe"))
+    #bot.st = datetime.datetime.now()
+
+
+#Load Extension Command
+@bot.command
+async def loadExt(extName : str):
+    #loading an Extension
+    try:
+        bot.load_extension(extName)
+    except (AttributeError, ImportError) as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await bot.say("{} Has arrived..".format(extName))
+
+#Unload Extension Command
+@bot.command
+async def unloadExt(extName : str):
+    #Unloading an Extension
+    bot.unload_extsion(extName)
+    await bot.say("{} Has left.".format(extName))
+
+# Reload extension command
+@bot.command()
+async def reloadExt(extName : str):
+    # Reloads an extension.
+    try:
+        tmp = await bot.say("Reloading extension {}".format(extName))
+        bot.unload_extension(extName)
+        bot.load_extension(extName)
+        await bot.edit_message(tmp, "Reloaded extension {}!".format(extName))
+    except Exception as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+
+# Reload startup command
+@bot.command()
+async def reloadBot():
+    for extension in start_up:
+        bot.unload_extension(extension)
+        try:
+            bot.load_extension(extension)
+            await bot.say("Reloaded {}!".format(str(extension)))
+        except Exception as e:
+            await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+
+#Setting up bot
+
+if __name__ == "__main__":
+    for extension in auto_load:
+        try:
+            bot.load_extension(extension)
+            print("{} Has arrived.".format(extension))
+        except Exception as e:
+            print("{}: {}".format(type(e).__name__, e))
+
+
+
+bot.run("Mjg4MzIxMTg4MTA1NTUxODcy.C5-OlA.mPmws1SbAjRGzN-fKLo45yrSn6s")
